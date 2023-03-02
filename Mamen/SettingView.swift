60dc80cc7a4847c47cc062b60bce30c6e8f3d980
chester_zhao <chester_zhao@icloud.com>
2023-03-02 23:30:38 +0800

    1.06
    Finish setting View frame work

--- Mamen/SettingView.swift
+++ Mamen/SettingView.swift
@@ -8,17 +8,37 @@
 import SwiftUI
 
 struct SettingView: View {
+    @State var show = false
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-        VStack {
-            Text("🧑🏻‍💻")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundColor(Color("main-green"))
-                .padding()
-            Text("Setting Page")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundColor(Color("main-green"))
+        NavigationStack {
+            List {
+                Section("Personal setting") {
+                    SettingElementView(icon: "person.fill", name: "Perosn", color: .blue)
+                }
+
+                Section("Personal setting") {
+                    
+                    NavigationLink(destination:
+                        LibaryView()
+                            .navigationTitle("haha")
+                    ) {
+                        SettingElementView(icon: "person.fill", name: "Perosn", color: .gray)
+                    }
+                    
+                }
+            }
+            .navigationBarTitle("Setting", displayMode: .inline)
+            .toolbar {
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image(systemName: "chevron.down")
+                }
+                Spacer()
+            }
+            .accentColor(Color("main-green"))
         }
     }
 }
@@ -26,5 +46,24 @@ struct SettingView: View {
 struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
         SettingView()
+    }
+}
+
+struct SettingElementView: View {
+    var icon: String
+    var name: String
+    var color: Color
+
+    var body: some View {
+        HStack {
+            RoundedRectangle(cornerRadius: 7, style: .continuous)
+                .frame(width: 32, height: 32)
+                .foregroundColor(color)
+                .overlay {
+                    Image(systemName: icon)
+                        .foregroundColor(.white)
+                }
+            Text(name)
+        }
     }
 }
